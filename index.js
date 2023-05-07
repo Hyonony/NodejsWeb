b6cf25602ecfbd3ea9c015444bad0dca0517119e
Hyonony <sic6688@naver.com>
2023-05-07 19:21:30 +0900

    mysql 연동 후 회원가입 Insert Data

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const mysql = require('mysql2');
 
 // body-parser 미들웨어를 사용하여 POST 데이터를 파싱합니다.
 const bodyParser = require('body-parser');
 dotenv.config();
 const httpPort = process.env.HTTP_PORT;
+
+// MySQL 연결 객체를 생성합니다.
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+// MySQL 연결 객체를 app.locals에 저장합니다.
+app.locals.connection = connection;
 
 // html 파일을 렌더링하기 위한 미들웨어 설정
 app.engine('html', require('ejs').renderFile);
@@ -20,15 +32,40 @@ app.get('/pages/signup', (req, res) => {
 
 // 회원 가입을 처리하는 미들웨어
 app.post('/signup', (req, res) => {
-    const username = req.body.username;
+    const userID = req.body.username;
     const password = req.body.password;
     const email = req.body.email;
-    // 여기서부터는 데이터베이스 등에 회원 정보를 저장하는 로직을 작성합니다.
-    // 생략
+  
+    const connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    });
+  
+    connection.connect((err) => {
+      if (err) throw err;
+      console.log('MySQL에 연결되었습니다.');
+    });
+  
+    const sql = `INSERT INTO users (userID, password, email) VALUES ('${userID}', '${password}', '${email}')`;
+  
+    connection.query(sql, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+    });
+  
+    connection.end();
+  
     res.send('회원 가입이 완료되었습니다.');
+  });
+
+// MySQL 연결 객체를 시작합니다.
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('MySQL에 연결되었습니다.');
 });
 
-// 404 에러 핸들링 미들웨어
 app.use((req, res, next) => {
     res.status(404).send('404 에러');
 });
